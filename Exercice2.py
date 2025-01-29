fdc3aa392af0137ec01db04b3535281b76b04917
Nathaniel Samson <202184320@cegepsherbrooke.qc.ca>
2025-01-29 14:05:26 -0500

    Exercice 2

---
+++ Exercice2.py
@@ -0,0 +1,12 @@
+# Demander à l'utilisateur d'entrer un nombre
+nombre_str = input("Entrer un nombre svp : ")
+
+# Convertir le nombre en entier
+nombre_entier = int(nombre_str)
+
+# Convertir le nombre en type flottant
+nombre_flottant = float(nombre_str)
+
+# Afficher les résultats
+print(f"Nombre en entier : {nombre_entier}")
+print(f"Nombre en flottant : {nombre_flottant}")
---
+++ Exercice3.py
@@ -0,0 +1,5 @@
+def fonction(a, b) :
+    c = a + b # Variable locale à cette fonction
+    return c
+
+print(fonction(3, 4))
