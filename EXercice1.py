dd5abf85ec52933f195dd787462ba1546effd468
Nathaniel Samson <202184320@cegepsherbrooke.qc.ca>
2025-01-29 13:35:03 -0500

    Create EXercice1.py

---
+++ EXercice1.py
@@ -0,0 +1,4 @@
+x = 10 #Type Int parce que c'est un bombre entier
+y = 3.14 # Type Float parce que c'est un nombre décimal
+z = "Bonjour" # Type str parce que c'est une chaine de caractères
+a = True # Type bool parce que c'est un Booléeen (True/False)
